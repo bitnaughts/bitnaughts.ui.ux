d441a5b09ef7a6f265bcdc1327497a866298c2f0
Brian <brianhungerman@gmail.com>
2019-12-10 18:02:02 -0800

    Enabling GameObject manipulation via interaction

--- Controller/InteractionController.cs
+++ Controller/InteractionController.cs
@@ -1,54 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InteractionController : MonoBehaviour {
 
-    List<InteractionObject> interactables = new List<InteractionObject>();
+    List<InteractionObject> interactables = new List<InteractionObject> ();
 
-	Vector3 click_position = new Vector3();
-    Vector3 world_position = new Vector3();
+    Vector3 click_position = new Vector3 ();
+    Vector3 world_position = new Vector3 ();
 
     Camera camera;
 
-	void Start () {
+    Task<string> data { get; set; }
+    InteractionObject current_interaction;
+
+    void Start () {
         camera = Camera.main;
-	}
-	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
+    }
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
             click_position = Input.mousePosition;
 
-            world_position = camera.ScreenToWorldPoint(Input.mousePosition);
+            world_position = camera.ScreenToWorldPoint (Input.mousePosition);
             print (click_position + " " + world_position);
 
             foreach (var interactable in interactables) {
-                if (PointHandler.GetDistance (new PointF(interactable.x, interactable.y), new PointF(world_position.x, world_position.y)) < interactable.radius) {
+                if (PointHandler.GetDistance (new PointF (interactable.point.x, interactable.point.y), new PointF (world_position.x, world_position.y)) < interactable.radius) {
                     print ("HIT: " + interactable.trigger);
-                    Referencer.system_id = int.Parse(interactable.trigger);
-                    SceneManager.LoadScene("System");
-                    break;
+
+                    if (interactable.trigger.Contains ("Planet")) {
+
+                        break;
+                    } else if (interactable.trigger.Contains ("Asteroid")) {
+
+                        data = Referencer.database.Mine (
+                            int.Parse (interactable.trigger.Split (' ') [1]), 0, 1
+                        );
+                        current_interaction = interactable;
+
+                        break;
+                    } else {
+                        Referencer.system_id = int.Parse (interactable.trigger);
+                        SceneManager.LoadScene ("System");
+                        break;
+                    }
+
                 }
+            }
+        }
+        if (data == null) {
+
+        } else {
+            if (data.IsCompleted) {
+                Debug.Log (data.Result);
+                if (data.Result.Contains ("Not")) {
+                    Destroy(current_interaction.obj);
+                }
+            } else {
+                Debug.Log (data.Status);
             }
         }
 
     }
-    public void Add (float x, float y, float radius, string trigger) {
-        Add(new InteractionObject(x, y, radius, trigger));
+    public void Add (PointF point, float radius, string trigger, GameObject obj) {
+        Add (new InteractionObject (point, radius, trigger, obj));
+    }
+    public void Add (float x, float y, float radius, string trigger, GameObject obj) {
+        Add (new InteractionObject (new PointF (x, y), radius, trigger, obj));
     }
     public void Add (InteractionObject interactable) {
-        interactables.Add(interactable);
+        interactables.Add (interactable);
     }
 }
 
 public class InteractionObject {
-    public float x, y;
+    public PointF point;
     public float radius;
     public string trigger;
-    public InteractionObject(float x, float y, float radius, string trigger) {
-        this.x = x;
-        this.y = y;
+    public GameObject obj;
+    public InteractionObject (PointF point, float radius, string trigger, GameObject obj) {
+        this.point = point;
         this.radius = radius;
         this.trigger = trigger;
+        this.obj = obj;
     }
 }
