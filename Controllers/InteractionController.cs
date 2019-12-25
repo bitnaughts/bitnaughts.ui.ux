78fd8d1bb092bd96673856a5347852eaa26c9d7d
Brian <brianhungerman@gmail.com>
2019-12-25 13:40:20 -0800

    Adding Contains, Add helper functions

--- Controllers/InteractionController.cs
+++ Controllers/InteractionController.cs
@@ -32,7 +32,8 @@ public class InteractionController : MonoBehaviour {
             print (click_position + " " + world_position);
 
             foreach (var interactable in interactables) {
-                if (PointHandler.GetDistance (new PointF (interactable.point.x, interactable.point.y), new PointF (world_position.x, world_position.y)) < interactable.radius) {
+                if (interactable.Contains (world_position)) {
+                    // if (PointHandler.GetDistance (new PointF (interactable.point.x, interactable.point.y), new PointF (world_position.x, world_position.y)) < interactable.radius) {
                     print ("HIT: " + interactable.trigger);
 
                     if (interactable.trigger.Contains ("Planet")) {
@@ -40,7 +41,7 @@ public class InteractionController : MonoBehaviour {
                         data = Referencer.database.Visit (
                             int.Parse (interactable.trigger.Split (' ') [1]), 0, 1
                         );
-                        current_interaction = interactable;
+                        // current_interaction = interactable;
 
                         break;
                     } else if (interactable.trigger.Contains ("Asteroid")) {
@@ -48,7 +49,7 @@ public class InteractionController : MonoBehaviour {
                         data = Referencer.database.Mine (
                             int.Parse (interactable.trigger.Split (' ') [1]), 0, 1
                         );
-                        current_interaction = interactable;
+                        // current_interaction = interactable;
 
                         break;
                     } else {
@@ -65,9 +66,9 @@ public class InteractionController : MonoBehaviour {
         } else {
             if (data.IsCompleted) {
                 Debug.Log (data.Result);
-                if (data.Result.Contains ("Not")) {
-                    Destroy (current_interaction.obj);
-                }
+                // if (data.Result.Contains ("Not")) {
+                //     Destroy (current_interaction.obj);
+                // }
             } else {
                 Debug.Log (data.Status);
             }
@@ -75,10 +76,13 @@ public class InteractionController : MonoBehaviour {
 
     }
     public void Add (PointF point, float radius, string trigger, GameObject obj) {
-        Add (new InteractionObject (point, radius, trigger, obj));
+        Add (new InteractionObject (point, new SizeF (radius, radius), trigger, obj));
     }
     public void Add (float x, float y, float radius, string trigger, GameObject obj) {
-        Add (new InteractionObject (new PointF (x, y), radius, trigger, obj));
+        Add (new InteractionObject (new PointF (x, y), new SizeF (radius, radius), trigger, obj));
+    }
+    public void Add (PanelObject panel) {
+        Add (new InteractionObject (panel.position, panel.bounds, "yes", null));
     }
     public void Add (InteractionObject interactable) {
         interactables.Add (interactable);
@@ -87,13 +91,19 @@ public class InteractionController : MonoBehaviour {
 
 public class InteractionObject {
     public PointF point;
-    public float radius;
+    public SizeF bounds;
     public string trigger;
     public GameObject obj;
-    public InteractionObject (PointF point, float radius, string trigger, GameObject obj) {
+    public InteractionObject (PointF point, SizeF bounds, string trigger, GameObject obj) {
         this.point = point;
-        this.radius = radius;
+        this.bounds = bounds;
         this.trigger = trigger;
         this.obj = obj;
+    }
+    public bool Contains (PointF target) {
+        return target.x > point.x - bounds.x / 2 &&
+            target.x < point.x + bounds.x / 2 &&
+            target.y > point.y - bounds.y / 2 &&
+            target.y < point.y + bounds.y / 2;
     }
 }
