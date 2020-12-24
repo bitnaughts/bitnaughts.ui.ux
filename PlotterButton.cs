aaf5805514d3a2f7782eb42d2eb1bdec7fca8416
Brian Hungerman <brianhungerman@gmail.com>
2020-12-24 00:06:30 -0800

    Up to date

--- PlotterButton.cs
+++ PlotterButton.cs
@@ -7,25 +7,74 @@ public class PlotterButton : MonoBehaviour
     // Start is called before the first frame update
     PlotterController controller;
     StructureController ship;
-    void Awake() {
+    void Awake()
+    {
         controller = GameObject.Find("PlotterOverlay").GetComponent<PlotterController>();
         ship = GameObject.Find("Ship").GetComponent<StructureController>();
     }
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnMouseUp()
     {
-        switch (this.name) {
+        switch (this.name)
+        {
+            /* Shift */
+            case "Up^":
+                ship.Move(controller.Selected(), new Vector2(0, 1));
+                controller.Select();
+                break;
+            case "Down^":
+                ship.Move(controller.Selected(), new Vector2(0, -1));
+                controller.Select();
+                break;
+            case "Right^":
+                ship.Move(controller.Selected(), new Vector2(1, 0));
+                controller.Select();
+                break;
+            case "Left^":
+                ship.Move(controller.Selected(), new Vector2(-1, 0));
+                controller.Select();
+                break;
+            /* Upsize */
+            case "Up+":
+                ship.Upsize(controller.Selected(), new Vector2(0, 1));
+                controller.Select();
+                break;
+            case "Down+":
+                ship.Upsize(controller.Selected(), new Vector2(0, -1));
+                controller.Select();
+                break;
+            case "Right+":
+                ship.Upsize(controller.Selected(), new Vector2(1, 0));
+                controller.Select();
+                break;
+            case "Left+":
+                ship.Upsize(controller.Selected(), new Vector2(-1, 0));
+                controller.Select();
+                break;
+            /* Downsize */
+            case "Up-":
+                ship.Downsize(controller.Selected(), new Vector2(0, 1));
+                controller.Select();
+                break;
+            case "Down-":
+                ship.Downsize(controller.Selected(), new Vector2(0, -1));
+                controller.Select();
+                break;
+            case "Right-":
+                ship.Downsize(controller.Selected(), new Vector2(1, 0));
+                controller.Select();
+                break;
+            case "Left-":
+                ship.Downsize(controller.Selected(), new Vector2(-1, 0));
+                controller.Select();
+                break;
             case "Rotate":
                 ship.Rotate90(controller.Selected());
+                controller.Select();
                 break;
             case "Ok":
                 controller.Deselect();
--- PlotterController.cs
+++ PlotterController.cs
@@ -5,96 +5,130 @@ using UnityEngine.UI;
 
 public class PlotterController : MonoBehaviour
 {
-    public GameObject Processor, Beam, Grid, Engine, Thruster, Gimbal, Hardpoint, Cannon, Sensor, Printer, Bulkhead;
-
-    GameObject placement_overlay;
-    ToggleGroup component_toggle_group;
+    public GameObject[] Prefabs;
+    GameObject placement_overlay, component_content;
+    ToggleGroup toggle_group;
+    Toggle[] toggles;
+    Text[] toggle_labels;
 
     StructureController ship;
-    InputField component_namer;
 
+    Text left_title, center_title, right_title;
+
+    InputField component_namer, interpreter;
+
+    CameraController camera_controller;
+
+    public string default_content = "\n None... \n\n To add, tap\n plotter grid.";
     public Sprite Overlay, OverlaySelected;
 
+    const string Components = "Components",
+        Syntax = "Syntax",
+        Arithmetic = "Arithmetic",
+        Flow_Control = "Flow Control",
+        Boolean = "Boolean",
+        Trigonometry = "Trigonometry",
+        Interactivity = "Interactivity";
+
+
+
+    string[] components_menu = { ">\tProcessor", ">\tGirder", ">\tGrid", ">\tBulkhead", ">\tPrinter", ">\tGimbal", ">\tEngine", ">\tThruster", ">\tCannon", ">\tSensor" },
+        syntax_init_menu = { "^\tScroll Up", "+\tAdd Above", ">\t" + Arithmetic, ">\t" + Flow_Control, ">\t" + Boolean, ">\t" + Trigonometry, ">\t" + Interactivity, "-\tDelete", "+\tAdd Below", "v\tScroll Down" },
+        syntax_arthimetic_menu = { ">\tSet", ">\tAdd", ">\tSubtract", ">\tAbsolute", ">\tMultiply", ">\tDivide", ">\tModulo", ">\tExponential", ">\tRoot", "-\tBack" },
+        syntax_flow_control_menu = { ">\tJump Label", ">\tJump", ">\tJump if equal", ">\tJump if not equal", ">\tJump if greater", ">\tJump if less", ">\t...", ">\t...", ">\t...", "-\tBack" },
+        syntax_boolean_menu = { ">\tSet", ">\tAnd", ">\tOr", ">\tNot", ">\tNand", ">\tNor", ">\tXnor", ">\t...", ">\t...", "-\tBack" },
+        syntax_trigonometry_menu = { ">\tSine", ">\tCosine", ">\tTangent", ">\tSecant", ">\tCosecant", ">\tCotangent", ">\tArcsine", ">\tArccosine", ">\tArctangent", "-\tBack" },
+        syntax_interactivity_menu = { ">\tFunction", ">\tPlot", ">\tPrint", ">\tPing", ">\tParse", ">\t...", ">\t...", ">\t...", ">\t...", "-\tBack" };
+
+
+    int num_toggles = 10;
+
+    float transition_time = 0f;
 
     void Start()
     {
         ship = GameObject.Find("Ship").GetComponent<StructureController>();
         placement_overlay = GameObject.Find("PlacementOverlay");
-        component_toggle_group = GameObject.Find("ComponentsBody").GetComponent<ToggleGroup>();
-        component_namer = GameObject.Find("ComponentNamer").GetComponent<InputField>();
+        toggle_group = GameObject.Find("Left").GetComponent<ToggleGroup>();
+        component_namer = GameObject.Find("RightInput").GetComponent<InputField>();
+        interpreter = GameObject.Find("RightView").GetComponent<InputField>();
+
+        left_title = GameObject.Find("LeftTitle").GetComponent<Text>();
+
+        camera_controller = GameObject.Find("Main Camera").GetComponent<CameraController>();
+
+        component_content = GameObject.Find("LeftViewContent");
+        component_content.GetComponent<Text>().text = default_content;
+
+        toggle_labels = new Text[num_toggles];
+        toggles = new Toggle[num_toggles];
+        for (int i = 0; i < num_toggles; i++)
+        {
+            toggles[i] = GameObject.Find(i + "").GetComponent<Toggle>();
+            toggle_labels[i] = GameObject.Find(i + "Label").GetComponent<Text>();
+        }
+
         Deselect();
     }
     string selected = "";
     bool clicked = false;
     string focused = "";
+    float recently_unfocused = 0f;
     float click_duration;
+
+    Vector2 selected_size, selected_min_size, pos;
     void Update()
     {
-        if (selected == "" && Input.mousePosition.x > Screen.width / 4 && Input.mousePosition.x < 3 * Screen.width / 4)
+        recently_unfocused -= Time.deltaTime;
+        transition_time -= Time.deltaTime;
+        pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.x = Mathf.Round(pos.x);
+        pos.y = Mathf.Round(pos.y);
+
+        if (focused == "") placement_overlay.transform.position = pos;
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.x = Mathf.Round(pos.x);
-            pos.y = Mathf.Round(pos.y);
-
-            foreach (var component in component_toggle_group.ActiveToggles())
+            clicked = true;
+            click_duration = Time.time;
+        }
+        if (Input.GetMouseButtonUp(0) && Time.time - click_duration < .5f)
+        {
+            if (selected != "" && Input.mousePosition.x > 3 * Screen.width / 4 && Input.mousePosition.y < 9 * Screen.height / 10)
             {
-                Vector2 min_size = GetMinimumSize(component.name);
-                
-                if (focused == "") placement_overlay.GetComponent<SpriteRenderer>().size = min_size;
-
-                if (min_size.x % 2 == 1) pos.x += .5f;
-                if (min_size.y % 2 == 1) pos.y += .5f;
-
-                if (Input.GetMouseButtonDown(0))
+                SwitchLeftView(Syntax);
+            }
+            if (selected == "" && recently_unfocused < 0 && Input.mousePosition.x > Screen.width / 4 && Input.mousePosition.x < 3 * Screen.width / 4)
+            {
+                if (focused != "") Select(focused);
+                else 
                 {
-                    clicked = true;
-                    click_duration = Time.time;
-                }
-                if (Input.GetMouseButtonUp(0) && Time.time - click_duration < .25f)
-                {
-                    if (focused != "") Select(focused);
-                    else
+                    foreach (var toggle in toggle_group.ActiveToggles())
                     {
-                        bool valid = true;
-                        Rect new_component = new Rect(pos.x, pos.y, min_size.x, min_size.y);
-                        foreach (var ship_component in ship.components.Values)
-                        {
-                            if (new_component.Overlaps(ship_component.rectangle))
-                            {
-                                valid = false;
-                            }
-                        }
-                        if (valid)
-                        {
-                            var component_gameObject = Instantiate(GetObject(component.name), pos, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
-                            int component_count = 1;
-                            while (ship.transform.Find("Rotator").Find(component.name + component_count) != null) component_count++;
-                            component_gameObject.name = component.name + component_count;
-                            component_gameObject.GetComponent<SpriteRenderer>().size = min_size;
-                            component_gameObject.transform.SetParent(ship.transform.Find("Rotator"));
-                            component_gameObject.GetComponent<ComponentController>().rectangle = new_component;
-
-                            // selected = component_gameObject.name;
-                            Select(component_gameObject.name);
-                        }
+                        GameObject object_reference = GetObject(toggle.name);
+                        
+                        var component_gameObject = Instantiate(object_reference, pos, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+                        int component_count = 1;
+                        while (ship.transform.Find("Rotator").Find(object_reference.name + component_count) != null) component_count++;
+                        component_gameObject.name = object_reference.name + component_count;
+                        component_gameObject.GetComponent<SpriteRenderer>().size = object_reference.GetComponent<ComponentController>().GetMinimumSize();
+                        component_gameObject.transform.SetParent(ship.transform.Find("Rotator"));
+                        Focus(component_gameObject.name);
                     }
                 }
             }
-
-
-            if (focused == "") placement_overlay.transform.position = pos;
         }
     }
-
-    public void Focus(string component, Vector2 position, Vector2 size)
+    public void Focus(string component) { Focus(component, false); }
+    public void Focus(string component, bool force_focus)
     {
-        if (this.selected == "")
+        if (this.selected == "" || force_focus)
         {
-            
             component_namer.text = component;
             focused = component;
-            placement_overlay.transform.position = position;
-            placement_overlay.GetComponent<SpriteRenderer>().size = size;
+
+            placement_overlay.transform.position = ship.GetPosition(component);
+            placement_overlay.GetComponent<SpriteRenderer>().size = ship.GetSize(component);
             placement_overlay.GetComponent<SpriteRenderer>().sprite = OverlaySelected;
         }
     }
@@ -102,19 +136,118 @@ public class PlotterController : MonoBehaviour
     {
         if (this.selected == "")
         {
-            component_namer.text = selected;
+            component_namer.text = "";
             focused = "";
+            recently_unfocused = .25f;
             placement_overlay.GetComponent<SpriteRenderer>().sprite = Overlay;
+
+            foreach (var toggle in toggle_group.ActiveToggles())
+            {
+                placement_overlay.GetComponent<SpriteRenderer>().size = GetObject(toggle.name).GetComponent<ComponentController>().GetMinimumSize();
+            }
         }
     }
 
-    public void Rename() 
+    public void Rename()
     {
         print(selected);
         print(component_namer.text);
         GameObject.Find(selected).name = component_namer.text;
         selected = component_namer.text;
         ship.Start();
+        Select();
+    }
+
+    public void SwitchLeftView(string title)
+    {
+        left_title.text = title;
+        transition_time = 1f;
+        switch (title)
+        {
+            case Syntax: 
+                SetActiveToggles(syntax_init_menu); break;
+            case Components: 
+                SetActiveToggles(components_menu); break;
+            case Arithmetic: 
+                SetActiveToggles(syntax_arthimetic_menu); break;
+            case Flow_Control: 
+                SetActiveToggles(syntax_flow_control_menu); break;
+            case Boolean: 
+                SetActiveToggles(syntax_boolean_menu); break;
+            case Trigonometry: 
+                SetActiveToggles(syntax_trigonometry_menu); break;
+            case Interactivity: 
+                SetActiveToggles(syntax_interactivity_menu); break;
+            default:
+                //Interpreter operand selection...
+
+                break;
+        }
+    }
+
+    public void SetActiveToggles(string[] toggle_labels_text)
+    {
+        for (int i = 0; i < num_toggles; i++)
+        {
+            toggles[i].isOn = false;
+            toggle_labels[i].text = toggle_labels_text[i];
+        }
+    }
+
+    public int GetActiveToggle()
+    {
+        foreach (var toggle in toggle_group.ActiveToggles())
+        {
+            return int.Parse(toggle.name);
+        }
+        return 0;
+    }
+    public void ToggleSwitched()
+    {
+        if (transition_time < 0) switch (left_title.text)
+        {
+            case Components:
+                Deselect(); break; 
+            case Syntax:
+                switch (GetActiveToggle())
+                {
+                    case 0:
+                        print("up");
+                        SwitchLeftView(Syntax);
+                        break;
+                    case 1:
+                        print("add up");
+                        SwitchLeftView(Syntax);
+                        break;
+                    case 7:
+                        print("delete");
+                        SwitchLeftView(Syntax);
+                        break;
+                    case 8:
+                        print("add down");
+                        SwitchLeftView(Syntax);
+                        break;
+                    case 9:
+                        print("down");
+                        SwitchLeftView(Syntax);
+                        break;
+                    default:
+                        SwitchLeftView(toggle_labels[GetActiveToggle()].text);
+                        break;
+                }
+                break;
+            default:
+                switch (GetActiveToggle())
+                {
+                    case 9:
+                        SwitchLeftView(Syntax);
+                        break;
+                    default:
+                        SwitchLeftView(toggle_labels[GetActiveToggle()].text);
+                    break;
+                }
+                break;
+        }
     }
 
     public void Deselect()
@@ -124,84 +257,98 @@ public class PlotterController : MonoBehaviour
 
         component_namer.text = selected;
         component_namer.interactable = false;
+        interpreter.interactable = false;
+        component_content.GetComponent<Text>().text = "\n Description of current component...\n Description of current component...\n Description of current component...";
         placement_overlay.GetComponent<SpriteRenderer>().sprite = Overlay;
         ship.EnableColliders();
+
+        recently_unfocused = .5f;
+
+        SwitchLeftView(Components);
+        foreach (var toggle in toggle_group.ActiveToggles())
+        {
+            placement_overlay.GetComponent<SpriteRenderer>().size = GetObject(toggle.name).GetComponent<ComponentController>().GetMinimumSize();
+        }
         foreach (Transform child in transform.Find("PlacementOverlay"))
         {
             child.gameObject.SetActive(false);
         }
     }
-    public void Select(string component)
+    public void Select()
+    {
+        Select(selected, ship.GetSize(selected), ship.GetMinimumSize(selected));
+    }
+
+    public void Select(string focused) 
+    {
+        Select(focused, ship.GetSize(focused), ship.GetMinimumSize(focused));
+    }
+    public void Select(string component, Vector2 component_size, Vector2 component_min_size)
     {
         this.selected = component;
 
         component_namer.text = selected;
         component_namer.interactable = true;
+        interpreter.interactable = true;
+        component_content.GetComponent<Text>().text = ship.ToString();
+        component_content.GetComponent<RectTransform>().sizeDelta = new Vector2(component_content.GetComponent<RectTransform>().sizeDelta.x, component_content.GetComponent<Text>().text.Split('\n').Length * 26);
+        placement_overlay.transform.position = ship.GetPosition(component);
+        placement_overlay.GetComponent<SpriteRenderer>().size = component_size;
         placement_overlay.GetComponent<SpriteRenderer>().sprite = OverlaySelected;
         ship.DisableColliders();
 
-        foreach (Transform child in transform.Find("PlacementOverlay"))
-        {
-            child.gameObject.SetActive(true);
-        }
+        SetPlacementOverlay(component_size, component_min_size);
+
     }
     public string Selected()
     {
         return selected;
     }
-    private Vector2 GetMinimumSize(string component)
+
+    public void SetPlacementOverlay(Vector2 component_size, Vector2 component_min_size)
     {
-        switch (component)
+        foreach (Transform child in transform.Find("PlacementOverlay"))
         {
-            case "Processor":
-                return new Vector2(4, 4);
-            case "Engine":
-                return new Vector2(5, 2);
-            case "Printer":
-                return new Vector2(3, 3);
-            case "Bulkhead":
-                return new Vector2(2, 6);
-            case "Beam":
-            case "Grid":
-            case "Thruster":
-            case "Gimbal":
-            case "Hardpoint":
-            case "Cannon":
-            case "Sensor":
-            default:
-                return new Vector2(2, 2);
+            child.gameObject.SetActive(true);
+            switch (child.name)
+            {
+                case "Up":
+                    child.localPosition = new Vector2(0, component_size.y / 2);
+                    break;
+                case "Down":
+                    child.localPosition = new Vector2(0, -component_size.y / 2);
+                    break;
+                case "Right":
+                    child.localPosition = new Vector2(component_size.x / 2, 0);
+                    break;
+                case "Left":
+                    child.localPosition = new Vector2(-component_size.x / 2, 0);
+                    break;
+                case "Rotate":
+                    child.localPosition = new Vector2(-component_size.x / 2 - 1, component_size.y / 2 + 1);
+                    break;
+                case "Ok":
+                    child.localPosition = new Vector2(component_size.x / 2 + 1, component_size.y / 2 + 1);
+                    break;
+                case "Delete":
+                    child.localPosition = new Vector2(component_size.x / 2 + 1, -component_size.y / 2 - 1);
+                    break;
+            }
+            foreach (Transform grandchild in child)
+            {
+                if ((grandchild.name == "Left-" || grandchild.name == "Right-") && component_size.x == component_min_size.x) grandchild.gameObject.SetActive(false);
+                else if ((grandchild.name == "Up-" || grandchild.name == "Down-") && component_size.y == component_min_size.y) grandchild.gameObject.SetActive(false);
+                else grandchild.gameObject.SetActive(true);
+            }
+
         }
     }
     private GameObject GetObject(string component)
     {
-        switch (component)
-        {
-            case "Processor":
-                return Processor;
-            case "Beam":
-                return Beam;
-            case "Grid":
-                return Grid;
-            case "Engine":
-                return Engine;
-            case "Thruster":
-                return Thruster;
-            case "Gimbal":
-                return Gimbal;
-            case "Hardpoint":
-                return Hardpoint;
-            case "Cannon":
-                return Cannon;
-            case "Sensor":
-                return Sensor;
-            case "Printer":
-                return Printer;
-            case "Bulkhead":
-                return Bulkhead;
-            default:
-                return null;
-
-        }
+        return Prefabs[int.Parse(component)];
     }
 
+    public void Print()
+    {
+    }
 }
